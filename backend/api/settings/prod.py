b69fd9135748e82fe255f931ad44c2db636be7c6
owinckle <oceanwinckler.rem@gmail.com>
2024-04-04 16:38:05 +0200

    config

--- backend/api/settings/prod.py
+++ backend/api/settings/prod.py
@@ -1,4 +1,25 @@
 from .base import *
+import os
 
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+	"userecho.onrender.com",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+MIDDLEWARE.insert(2, "whitenoise.middleware.WhiteNoiseMiddleware")
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, "static"),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
